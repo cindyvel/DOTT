87b38db64069e75227d6df33e8fde83beca08303
cindyvel <86742389+cindyvel@users.noreply.github.com>
2021-07-21 16:00:25 -0500

    Create convert.rb

---
+++ cidr_convert_api/convert.rb
@@ -0,0 +1,17 @@
+# aa
+class CidrMaskConvert
+  def cidr_to_mask(val)
+    val
+  end
+
+  def mask_to_cidr(val)
+    val
+  end
+end
+
+class IpValidate
+  def ipv4_validation(val)
+    true
+  end
+end
+© 2021 GitHub, Inc.
